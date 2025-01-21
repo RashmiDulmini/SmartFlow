a994a94ee05a6cde75c22e8708cc9b0be45a77bf
Navodya De Silva <kdnavodya7@gmail.com>
2025-01-21 22:51:56 +0530

    Firebase Configuration
    Firebase Configuration and initialize in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:smartflow/firebase_options.dart';
 import 'package:smartflow/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,  // Use the generated options for platform-specific configuration
+  );
+
   runApp(MyApp());
 }
 
